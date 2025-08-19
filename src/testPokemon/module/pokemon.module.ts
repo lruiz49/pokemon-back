99e9903e57d16e2ec08eda934d5923960a4b2990
lruiz49 <lruiz1610@gmail.com>
2025-08-19 18:30:52 -0300

    refactor: remove unused Pokemon module, controller, and DTO files

--- src/testPokemon/module/pokemon.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { PokemonController } from "../controller/pokemon.controller";
-
-
-@Module({
-  controllers: [PokemonController],
-})
-export class PokemonModule {}
